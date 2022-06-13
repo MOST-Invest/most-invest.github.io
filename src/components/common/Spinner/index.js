32148ae69e7044c9f15b6df33ff83368476e301e
Madeleine <madeleine.schonemann@gmail.com>
2022-06-13 20:43:37 +0200

    Updates

--- src/components/common/Spinner/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { IconSpinner } from "../SvgIcon";
-import * as styles from "./spinner.module.css";
-
-const Spinner = () => <IconSpinner className={styles.spinner}></IconSpinner>;
-
-export default Spinner;
--- src/pages/404.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import NotFound from "../components/pages/NotFound"
-
-const NotFoundPage = () => {
-  return <NotFound></NotFound>
-}
-
-export default NotFoundPage
